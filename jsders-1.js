b27e36d2e9efe4bcf603aed4fce8d590bf7056ca
omerfarukkpala <omerfarukkpala@gmail.com>
2022-05-24 16:29:07 +0300

    js uygulaması yaıldı

--- jsders-1.js
+++ jsders-1.js
@@ -7,3 +7,26 @@ var age = 27;
 console.log(age);
 
 //Tahmin edileceği üzere ekran çıktımız bu sefer 'age' degiskenimizin turu
+
+
+var bos;
+console.log(bos);
+
+//İcerisine deger atanmamıs ve dolayısıyla tipi olmayan bir degiskende sizce ne yazacaktır?
+//Javascript bu tur tipsiz ve ici bos degiskenlere tanımlanmamıs yani 'undefined' tipini veriyor.
+
+var bosamanull=null;
+console.log(bosamanull);
+
+//Bu degiskenimizin degeri var ancak null atanmıs,bu gibi durumlarda console bize 'object'yazdırır.
+
+
+age = 28;
+console.log(age);
+
+//Yukarıda degerımız 16ydı ancak artık 17 olarak ekrana verilecektir.
+
+yas ='yirmisekiz';
+console.log(yas);
+
+//Ve hatta int olan datamı bu sekilde stringe bile cevirebilirim.Ekran cıktısı 'yirmisekiz' olacaktır.
