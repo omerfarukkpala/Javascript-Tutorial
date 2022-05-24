2aae705022bbead0b84bf5bc177679b68f06d7fa
omerfarukkpala <omerfarukkpala@gmail.com>
2022-05-24 14:45:58 +0300

    isim ve yaş yazdırıldı

--- jsders-1.js
+++ jsders-1.js
@@ -1,1 +1,9 @@
-console.log('faruk')
+
+var nam = 'faruk';
+console.log(nam);
+//Ekran çıktımız 'name'degiskeninin turu olan 'String' olacaktır .
+
+var age = 27;
+console.log(age);
+
+//Tahmin edileceği üzere ekran çıktımız bu sefer 'age' degiskenimizin turu
