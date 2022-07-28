fe2872cf4badbc7c780febb5c74369b256eec460
omerfarukkpala <omerfarukkpala@gmail.com>
2022-07-28 16:47:03 +0300

    js-4 ve js-4-index 3 dosyalarının örneği yapılandırıldı.

---
+++ js-4.js
@@ -0,0 +1,14 @@
+
+
+
+var firstName,secondName,fullName;
+
+firstName = prompt("Adınız nedir: ");
+
+secondName= prompt("Soyadınız nedir?: ");
+
+fullName= firstName + " " + secondName;
+
+alert ("Merhaba, " + fullName);
+
+
