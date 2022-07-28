00a5a06d9298e38207c9d3daec72fdeeadae65a4
omerfarukkpala <omerfarukkpala@gmail.com>
2022-07-28 16:39:17 +0300

    input olarak adı ve soyadı alındı

--- js-3.js
+++ js-3.js
@@ -3,3 +3,12 @@
 //verelim.
 
 
+var firstName,secondName,fullName;
+
+firstName = prompt("Adınız nedir: ");
+
+secondName= prompt("Soyadınız nedir?: ");
+
+fullName= firstName + " " + secondName;
+
+alert ("Merhaba, " + fullName);
