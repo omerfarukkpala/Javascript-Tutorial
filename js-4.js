4a61dcaeda699ce9aaae3c240a60c1865b70b376
omerfarukkpala <omerfarukkpala@gmail.com>
2022-07-28 16:57:32 +0300

    birinci ve ikinci sayi parseInt ile toplama yapıldı

--- js-4.js
+++ js-4.js
@@ -1,14 +1,14 @@
 
+//var firstName,secondName,fullName;
+//firstName = prompt("Adınız nedir: ");
+//secondName= prompt("Soyadınız nedir?: ");
+//fullName= firstName + " " + secondName;
+//alert ("Merhaba, " + fullName);
 
 
-var firstName,secondName,fullName;
-
-firstName = prompt("Adınız nedir: ");
-
-secondName= prompt("Soyadınız nedir?: ");
-
-fullName= firstName + " " + secondName;
-
-alert ("Merhaba, " + fullName);
-
+var number1,number2,sum;
+number1= prompt("Birinci sayıyı giriniz: ");
+number2= prompt("İkinci sayıyı giriniz: ");
+sum= parseInt(number1) + parseInt(number2);
+alert= ("Toplam: " + sum);
 
