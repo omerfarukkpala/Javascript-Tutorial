c315325abea6f26a5deb52dea3c01966d5184a8b
omerfarukkpala <omerfarukkpala@gmail.com>
2022-07-29 09:52:09 +0300

    sayıların eşitlik kontrolü

--- js-5.js
+++ js-5.js
@@ -21,3 +21,18 @@ var x =4 * 3* 5 ;
 console.log(x);
 
 
+var x =5;
+var y =6;
+
+if (x=y){
+    // Her zaman doğru olur.
+    console.log(x);
+}
+else{
+    alert("x ve y eşit değil");
+}
+
+/*x ve ynin esitligini degerlendiren if-else yapısı yaptık.Eger x ve y sayıca esitse
+ İf bloguna,degıllerse de else bloguna girecektir.
+ (==) ile gosterilen cıft esıttır sadece sayıları
+ (===) ile gosterılen uc esıttır ise Veri tiplerini ve sayıların esıtlıgını ınceler. */
