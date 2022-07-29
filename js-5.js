67552033d6508216ebfc476886fc94efad35b118
omerfarukkpala <omerfarukkpala@gmail.com>
2022-07-29 11:09:39 +0300

    Increment

--- js-5.js
+++ js-5.js
@@ -36,3 +36,36 @@ else{
  İf bloguna,degıllerse de else bloguna girecektir.
  (==) ile gosterilen cıft esıttır sadece sayıları atar
  (===) ile gosterılen uc esıttır ise Veri tiplerini ve sayıların esıtlıgını ınceler. */
+
+
+ //Modulus Operator %
+
+ var year=2022;
+var kalan = year % 5;
+console.log(kalan);
+
+var kalan = x % 6; //Mod operatorumuz bize bolundugu sayıdan kalan sonucu verir
+
+console.log(kalan);
+
+//Increment *****************************************************************************************
+
+var x=x+1;
+x +=1;
+x++;
+++x;
+
+
+var x =4;
+console.log(x++);
+console.log(x);
+
+
+x--; //x ten bir çıkarttık  ve degerını 6 iken 5 yaptık.
+console.log(x);//ekrana 5 yazdırır.
+console.log(x++);//x in degerını bir sonrakı yazdırma ıslemı ıcın 1 arttırmıs olduk fakat değeri 5 yaptık.
+
+console.log(x);//1 oncekı satırda arttırılma ıslemı bu satıra devretti artık x'in degerı 6.
+console.log(--x);//Onemlı olan eksilerin ve artıların yeri goruldugu gibi burda eksi x'in onunde 
+//yani yapılan bu azaltma islemi bu satıra dahıl oluyor x degerı 5 yazacaktır ekrana.
+console.log(x++);//x degeri 5 olduğu için x'in degeri 6 yazacaktır.
