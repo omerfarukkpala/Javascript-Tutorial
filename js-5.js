9ac1b860de61ee813956f70677380aecaec6bdfb
omerfarukkpala <omerfarukkpala@gmail.com>
2022-07-29 09:45:54 +0300

    assigment operatörler uygulandı

--- js-5.js
+++ js-5.js
@@ -4,17 +4,20 @@
 var x =5;
 console.log(x);
 
-x += 6;
+x += 6; //Toplama islemini bu şekilde gosterebiliriz.
 console.log(x);
 
-x -= 4;
+x -= 4; //Cikarma islemini bu şekilde gosterebiliriz.       
 console.log(x);
 
-x *= 3;
+x *= 3; //Carpma islemini bu sekilde gosterebiliriz.
 console.log(x);
 
-x /= 7;
+x /= 7; //Bölme islemini bu sekilde gosterebiliriz.
+
 console.log(x);
 
 var x =4 * 3* 5 ;
 console.log(x);
+
+
