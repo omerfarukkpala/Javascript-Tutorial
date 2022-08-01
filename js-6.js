45015ff197ac8b752031b5c1823ff777304b94ca
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-01 09:09:21 +0300

    if yapıları sorgulandı

--- js-6.js
+++ js-6.js
@@ -13,3 +13,15 @@ else {
   } else {
       console.log("x değeri 5'den küçük");
   }
+
+  if(x > 5) {
+      console.log("x değeri 5'den büyük");
+  } 
+
+  if(x <5) {
+      console.log("x değeri 5'den küçük");
+  }
+
+  if(x == 5) {
+        console.log("x değeri 5");
+ }   // x değeri 5 olarak eşitse yapılacak işlemler
