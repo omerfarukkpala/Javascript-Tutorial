63323e0622d20c9d4d949f6690fbdd8b534cda19
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-01 12:42:00 +0300

    && mantıksal operatörü

--- js-7.js
+++ js-7.js
@@ -3,3 +3,22 @@
 //AND (&&) önermelerin 2side doğru olmalı 
 //OR (||) önermelerin 1side doğru olmalı
 
+// Not :true false çevrimi yapar
+
+var age = 18;
+
+if(age >= 20 && age <= 50){
+    console.log("Üye Olduğunuz için teşekkür ederiz");
+} else{
+    console.log("Üye olamazsınız");
+}
+
+
+/* && and operatörü
+doğru && doğru true
+yanlış && doğru= yanlış
+doğru && yanlış =yanlış
+yanlış && yanlış = yanlış */
+
+
+
