db068322a84d7e08f05e9d228505fc8c1dda8a65
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-02 17:50:29 +0300

    iç içe geçmiş if yapısı uygulandı

--- js-8.js
+++ js-8.js
@@ -11,9 +11,27 @@ if(a>b && a>c){
         alert(a+  " "+ b +" "+ c);
 
     }else {
-        alert(a+  " "+ b +" "+ c);
+        alert(a+  " "+ c +" "+ b);
 
     }
 }
 
+else if(b>a && b>c){
+    if(a>c){
+        alert(b+  " "+ a +" "+ c);
 
+    }else {
+        alert(b+  " "+ c +" "+ a);
+
+    }
+}
+
+else if(c>a && c>b){
+    if(a>b){
+        alert(c+  " "+ a +" "+ b);
+
+    }else {
+        alert(c+  " "+ b +" "+ a);
+
+    }
+}
