7400d9525f5c2534bb32a71567f085cc368507f7
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-02 17:43:48 +0300

    3 sayı karşılaştırma

--- js-8.js
+++ js-8.js
@@ -1,2 +1,19 @@
 
 /* IF ELSE ve Mantıksal Operatörler Uygulama */
+
+var a= 4;
+var b= -3;
+var c= 6;
+
+if(a>b && a>c){
+
+    if( b>c){
+        alert(a+  " "+ b +" "+ c);
+
+    }else {
+        alert(a+  " "+ b +" "+ c);
+
+    }
+}
+
+
