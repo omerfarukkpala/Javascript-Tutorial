550c779a14945f9128591885ae9a0f0410341cc4
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-03 14:34:13 +0300

    switch case

---
+++ js-9.js
@@ -0,0 +1,18 @@
+/* Switch Case Yapısı 
+switch(kontrol edilecek durum){
+
+    case 1:
+        kod çalıştır;
+        break;
+    case 2:
+        kod çalıştır;
+        break;
+    .....
+
+    default:
+        kod çalıştır;
+        
+
+}
+
+*/
