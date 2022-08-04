a0b97b8924ea7682802fcb208485b9c06950bbfd
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-04 11:58:49 +0300

    function giriş

--- js-10.js
+++ js-10.js
@@ -5,4 +5,24 @@ function fonksiyon_adi(parametreler) {
 }
 */
 
+V1=80;
+t1=4;
+x1=V1*t1;
+console.log(x1);
+
+
+v2=65;
+t2=6;
+x2=v2*t2;
+console.log(x2);
+
+function findX (t,V) {
+    var x=V*t;
+    return x;
+}
+var yol =findX(4,50);
+console.log(yol);
+
+var yol2 =findX(7,63);
+console.log(yol2);
 
