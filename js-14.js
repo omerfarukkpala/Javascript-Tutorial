c64b56dedc7dec8142700c3311445e824f1d020d
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-04 17:45:58 +0300

    vize final geçti kaldı uygulaması

--- js-14.js
+++ js-14.js
@@ -1,1 +1,13 @@
 /* Fonksiyon Uygulama */
+
+var vize = prompt("Vize notunuzu giriniz");
+var final = prompt("Final notunuzu giriniz");
+
+function dersSonucu(vize, final) {
+    var dersNotu = parseInt(vize * 0.4) + (final * 0.6);
+    if (sonuc >= 50) {
+        return "Geçti";
+    } else {
+        return "Kaldı";
+    }
+}
