816fb5a58a36a7794a232e8a2a9f7fecb7ced245
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-04 17:35:13 +0300

    daire cevresi ve daire alanı hesaplama

--- js-13.js
+++ js-13.js
@@ -2,3 +2,19 @@
 /*
 Fonksiyonlar  Uygulama
 */
+
+//cevre=2 * 3.14 * r;
+
+var yaricap =prompt("Yarıçapı giriniz");
+
+function daireCevreAlan(yaricap){
+
+    var cevre = parseInt(2 * Math.PI * yaricap);
+    alert("Daire çevresi: " + cevre);
+
+    var alan = parseInt(Math.PI * Math.pow(yaricap,2));
+    alert("Daire alanı: " + alan);
+
+}
+
+daireCevreAlan(yaricap);
