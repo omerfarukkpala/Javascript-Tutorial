5b04e2bb41141f463a4999e3225647561cf43ec6
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-04 16:52:49 +0300

    teorik fonksiyonlar bitti

--- js-12.js
+++ js-12.js
@@ -8,9 +8,23 @@ SCOPE
 var x ; //global variable
 
 function samplefunction(){
-    var x =55; //local variable
+    var x =05; //local variable
     console.log(x);
 }
  
 samplefunction();
-  
+
+console.log(x);
+
+
+function fonk1(){
+   //kodlar
+}
+
+if (x>7){
+  //.....  fonk1();
+}
+
+var fonk2=function(){
+    //kodlar
+}
