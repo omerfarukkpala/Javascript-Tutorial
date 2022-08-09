493b7ddf23a8636a7a1607fada5e56685f001101
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-09 09:26:17 +0300

    array çıktısı alındı

--- js-16.js
+++ js-16.js
@@ -1,6 +1,11 @@
 
 /* Javascript Array yapısı  */
 
-var name = "Nando";
-var name2= "Pala";
+//var name = "Nando";
+//var name2= "Pala";
+
+
+var names=['Nando','Pala','Faruk'];
+console.log(names);
+
 
