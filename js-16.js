a44d9f982be1db4da68ed35fdc49bb39d4f0c24f
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-09 09:36:21 +0300

    belli fonksiyonları kontrol edilmesi

--- js-16.js
+++ js-16.js
@@ -8,4 +8,27 @@
 var names=['Nando','Pala','Faruk'];
 console.log(names);
 
+var ages = [20,30,40];
+console.log(ages);
+
+var names2 =new Array('Nando','Pala','Faruk');
+
+console.log(names2);
+
+
+var bilgiler =['Ömer','Faruk' ,'Pala','Programmer',false];
+console.log(bilgiler);
+
+
+bilgiler.push('Yazılım Geliştiricisi');
+console.log(bilgiler);
+
+bilgiler.unshift('Mr.');
+console.log(bilgiler);
+
+bilgiler.pop();
+console.log(bilgiler);
+
+bilgiler.shift();
+console.log(bilgiler);
 
