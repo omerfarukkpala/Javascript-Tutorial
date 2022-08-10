9147542759807f79684215564c195ae5b37824d9
omerfarukkpala <omerfarukkpala@gmail.com>
2022-08-10 14:57:07 +0300

    array ile ilgili basit toplam ve çarpım  örnek yapıldı

--- js-17.js
+++ js-17.js
@@ -1,2 +1,12 @@
 
 var myArray =[4,7,5,2] ,toplam =0,carpim =1;
+
+for(var i=0; i<myArray.length; i++){
+    toplam += myArray[i];
+    carpim *= myArray[i];
+}
+
+//toplam ve çarpım yapıldı basit bir örnekle incelendi
+console.log("Toplam: " + toplam + "Çarpım: "+carpim);
+
+
