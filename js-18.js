03db25366e0f8ae2da5ee0a0eb11e547368122aa
omerfarukkpala <omerfarukkpala@gmail.com>
2022-12-02 15:26:48 +0300

    nesne yapısı

--- js-18.js
+++ js-18.js
@@ -1,2 +1,13 @@
 //
-// object  
+// object nesne yapısı
+
+var MyArray=['Ömer','Pala','Faruk'];
+console.log(MyArray[0]);
+
+var player1={
+    name:'Can',
+    lastName:'Ömer',
+    age: 35,
+    goal: 7,
+}
+
