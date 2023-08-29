fd5fec96f033be1073032b5233eadb655e817054
= <=>
2023-08-29 12:56:54 +0300

    3 kitaptan hangisinin  okunup okunmadığını bulalım

---
+++ js-20.js
@@ -0,0 +1,20 @@
+//3 kitaptan hangisinin okunup hangisinin okunmadığı ile ilgili bir ugulama yapalım.
+var library = [ 
+  {
+    title: 'İnce Memed',
+    author:'Yaşar Kemal',
+    status: true
+  },
+  {
+    title: 'Kuyucaklı Yusuf',
+    author:'Sebahattin Ali',
+    status: true
+
+  },
+  {
+    title: 'Memleketimden İnsan Manzaraları',
+    author:'Nazım Hikmet Ran',
+    status: false
+  } ];
+
+ // okunan okunmayan kitaplar hakkında bilgi veren 
