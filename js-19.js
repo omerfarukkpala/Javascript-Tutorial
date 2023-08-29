33d1055138d497d78f5843ffdeaf39c5ed474105
= <=>
2023-08-29 12:30:21 +0300

    js-19 fonksiyon çağırdık

--- js-19.js
+++ js-19.js
@@ -5,6 +5,11 @@ var player1={
     name:'Faruk',
     lastName:'Ömer',
     age: 26,
+    childs:['Nando','Icardi','Lukaku'],
+    fullName:function(){
+        return player1.name+" "+player1.lastName;
+    }
    
 }
-// console.log(player1);
+console.log(player1.fullName()); //Faruk Ömer
+// console.log(player1.childs[0]); //Nando
