43c5b9797b4686633e1f648540db09815ef7ca3e
= <=>
2023-08-29 12:44:05 +0300

    fonksiyon nesne içinde kullanıldı

--- js-19.js
+++ js-19.js
@@ -1,15 +1,34 @@
 
 //objects nesne yapısı bölüm2
 
+// var player1={
+//     name:'Faruk',
+//     lastName:'Ömer',
+//     age: 26,
+//     childs:['Nando','Icardi','Lukaku'],
+//     fullName:function(){
+//         return player1.name+" "+player1.lastName;
+//     } //nesnenin kendi yerine this kullanarak yapılması doğru olacaktır.
+   
+// }
+// console.log(player1.fullName()); //Faruk Ömer
+// // console.log(player1.childs[0]); //Nando
+
+
+
 var player1={
     name:'Faruk',
     lastName:'Ömer',
     age: 26,
     childs:['Nando','Icardi','Lukaku'],
     fullName:function(){
-        return player1.name+" "+player1.lastName;
-    }
-   
+        return this.name + " " + this.lastName;
+    } //nesnenin kendi yerine this kullanarak yapılması doğru olacaktır.
+   //this nesnenin kendisini ifade eder.
 }
 console.log(player1.fullName()); //Faruk Ömer
-// console.log(player1.childs[0]); //Nando
+
+var fullName =function(){
+    return this.name + " " + this.lastName;
+}
+// birbirinin aynısı fonksiyonu nesne içinde kullanmak istiyorsak
