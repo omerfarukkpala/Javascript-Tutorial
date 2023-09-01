71c7a5bb213cd97d0af0abc892d1ed704b80e09e
= <omerfarukkpala@gmail.com>
2023-09-01 16:03:10 +0300

    .

---
+++ js-24.js
@@ -0,0 +1,5 @@
+//console.log(document.URL); // Path: js-24.js
+//console.log(document.dir);
+//console.log(document.title);
+//document.title = "Uygulamalı JavaScript Dersleri";
+console.log(document.body);
