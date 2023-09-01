82a2e792026af6c44e5b8946e87f5018ca9a9be0
= <omerfarukkpala@gmail.com>
2023-09-01 16:07:05 +0300

    document nesnesi js-24 için çalışma yapıldı.

--- js-24.js
+++ js-24.js
@@ -3,3 +3,7 @@
 //console.log(document.title);
 //document.title = "Uygulamalı JavaScript Dersleri";
 console.log(document.body);
+console.log(document.head);
+console.log(document.links);
+//document nesnesi ile alakalı bir çok özellik ve metod bulunmaktadır.
+console.log(document.doctype);//<!DOCTYPE html>
