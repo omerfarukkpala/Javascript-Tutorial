00bc7bd92b80d03e779021c58b54329c69612213
= <omerfarukkpala@gmail.com>
2023-09-01 16:41:41 +0300

    dom .

---
+++ js-25.js
@@ -0,0 +1,19 @@
+//console.log(document.getElementById('myHeader'));
+
+// var myElement = document.getElementById('myHeader');
+// console.log(myElement);
+
+// myElement.textContent = "New Junbotron";
+// myElement.innerText = 'Yeni Junbotron';
+// myElement.innerHTML = '<h3>Küçük Junbotron</h3>';
+
+// console.log(myElement);
+
+// myElement.style.backgroundColor = 'red';
+
+var myClass = document.getElementsByClassName('nav-link');
+console.log(myClass);
+
+console.log(myClass[1]);//2. indexi verir
+
+myClass[1].textContent = 'İletişim';
