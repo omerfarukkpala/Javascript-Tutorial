15d2537385449e6510e4543c440af03e14bab70d
= <omerfarukkpala@gmail.com>
2023-09-06 12:05:22 +0300

    .

--- js-29.js
+++ js-29.js
@@ -6,3 +6,12 @@
 
 var myElement =document.querySelector('button');
 myElement.addEventListener('click', function1);
+myElement.addEventListener('click', function2);
+
+function function1() {
+  alert('hello');
+}
+
+function function2() {
+  alert('world');
+}
