59656f91a3486922c329fa807250a8a75d66727b
= <omerfarukkpala@gmail.com>
2023-09-06 11:26:17 +0300

    The most important things for us are classes and IDs.

---
+++ js-27.js
@@ -0,0 +1,12 @@
+var myNode =document.querySelector('#myHeader');
+console.log(myNode);
+// myNode.style.color ='red';
+myNode .removeAttribute('id');
+myNode.setAttribute('id','Pala');
+
+myNode.hasAttribute('id');
+myNode.classList.length;
+
+myNode.classList.remove('nav-item');
+
+myNode.classList.toggle('hidden')
