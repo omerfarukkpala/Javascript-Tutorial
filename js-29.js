4a7cc898761ce97e0136dec5af93dcdfd7e7e7a1
= <omerfarukkpala@gmail.com>
2023-09-06 12:04:54 +0300

    events1.

---
+++ js-29.js
@@ -0,0 +1,8 @@
+// var myElement = document.querySelector(' button');
+
+// myElement.onclick = function() {
+//   alert('hello');
+// };
+
+var myElement =document.querySelector('button');
+myElement.addEventListener('click', function1);
