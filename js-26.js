06f16aa7bd786cc8d79b89205437b7440a36fc32
= <omerfarukkpala@gmail.com>
2023-09-06 11:15:29 +0300

    dom selection querySelector

---
+++ js-26.js
@@ -0,0 +1,9 @@
+// var myElement = document.querySelector('#myHeader');
+// console.log(myElement);
+
+// var myElement = document.querySelector('.nav-item:nth-child(2)');
+// console.log(myElement);
+
+// myElement.style.color ='red';
+ var myElements= document.querySelectorAll('.nav-item');
+ console.log(myElements);
